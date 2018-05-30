3f2bda11db83e9e60b296443bb1c082fb520dbc5
Takahisa HAMAJI <hamaji_takahisa@kurusugawa.jp>
2018-05-30 22:00:54 +0900

    エラーメッセージなおした

--- result.go
+++ result.go
@@ -22,7 +22,7 @@ func (r *Result) String() string {
 func (r *Result) Scan(v interface{}) error {
 	rv := reflect.ValueOf(v)
 	if rv.Kind() != reflect.Ptr {
-		return errors.New("cmd: Scan(non-pointer " + rv.Type().String() + ")")
+		return errors.New("gexec: Scan(non-pointer " + rv.Type().String() + ")")
 	}
 	return decode(string(r.stdout), rv.Elem())
 }
@@ -36,26 +36,26 @@ func decode(s string, v reflect.Value) error {
 		case "0", "f", "F", "false", "FALSE", "False", "n", "N", "no", "NO", "No", "off", "OFF", "Off":
 			v.SetBool(false)
 		}
-		return errors.New("cmd: cannot decode " + v.Kind().String() + ": " + s)
+		return errors.New("gexec: cannot decode " + v.Kind().String() + ": " + s)
 
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		n, err := strconv.ParseInt(strings.TrimSpace(s), 10, 64)
 		if err != nil || v.OverflowInt(n) {
-			return errors.New("cmd: cannot decode " + v.Kind().String() + ": " + s)
+			return errors.New("gexec: cannot decode " + v.Kind().String() + ": " + s)
 		}
 		v.SetInt(n)
 
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
 		n, err := strconv.ParseUint(strings.TrimSpace(s), 10, 64)
 		if err != nil || v.OverflowUint(n) {
-			return errors.New("cmd: cannot decode " + v.Kind().String() + ": " + s)
+			return errors.New("gexec: cannot decode " + v.Kind().String() + ": " + s)
 		}
 		v.SetUint(n)
 
 	case reflect.Float32, reflect.Float64:
 		n, err := strconv.ParseFloat(strings.TrimSpace(s), v.Type().Bits())
 		if err != nil || v.OverflowFloat(n) {
-			return errors.New("cmd: cannot decode " + v.Kind().String() + ": " + s)
+			return errors.New("gexec: cannot decode " + v.Kind().String() + ": " + s)
 		}
 		v.SetFloat(n)
 
@@ -67,7 +67,7 @@ func decode(s string, v reflect.Value) error {
 		// reflect.Array, reflect.Chan, reflect.Func,reflect.Interface,
 		// reflect.Map, reflect.Ptr, reflect.Slice, reflect.Struct,
 		// reflect.UnsafePointer
-		return errors.New("cmd: unsupported type: " + v.Kind().String())
+		return errors.New("gexec: unsupported type: " + v.Kind().String())
 	}
 
 	return nil
